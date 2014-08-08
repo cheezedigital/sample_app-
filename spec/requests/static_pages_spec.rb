17f818b425299457f29d9282e1215338904104b4
Justin Veirs <justinveirs@gmail.com>
2014-08-08 17:11:02 -0600

    I'm actually happy with my tests. Understandable, and passing

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -5,21 +5,22 @@ describe "StaticPages" do
   # let(:base_title) { "Ruby on Rails Tutorial Sample App "}
 
   describe "Home Page" do
+
+    before { visit root_path }
+
     it "should have the content Sample App" do
-      visit root_path
       expect(page).to have_content('Sample App')
     end
     it "should have the base title" do
-      visit root_path
       expect(page).to have_title("Ruby on Rails Tutorial Sample App")
     end
     it "should not have a custom page title" do
-      visit root_path
       expect(page).not_to have_title(' | Home')
     end
   end
 
   describe "Help page" do
+
     it "should have content Help" do
       visit help_path
       expect(page).to have_content('Help')
@@ -31,6 +32,7 @@ describe "StaticPages" do
   end
 
   describe "About page" do
+
     it "should have content About Us" do
       visit about_path
       expect(page).to have_content('About Us')
@@ -42,6 +44,7 @@ describe "StaticPages" do
   end
 
   describe "Contact Page" do
+
     it "should have the content Contact" do
       visit contact_path
       expect(page).to have_content('Contact')
