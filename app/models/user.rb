6acd18f321edd9eac926b4395b9688fe4a6d5928
Justin Veirs <justinveirs@gmail.com>
2014-08-14 12:10:43 -0400

    migrations, and model updates

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ActiveRecord::Base
+  before_save { self.email = email.downcase }
   attr_accessor :name, :email
-  validates(:name, presence: true)
+  validates :name, presence: true, length: { maximum: 50 }
+  validates :email, presence: true
 end
