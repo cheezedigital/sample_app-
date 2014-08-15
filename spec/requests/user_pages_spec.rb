9354f1d70d49cf7e1afbb3b83cd9150999779805
Justin Veirs <justinveirs@gmail.com>
2014-08-15 18:44:54 -0400

    content for user

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -10,4 +10,11 @@ describe "UserPages" do
     it { should have_content('Sign Up') }
     it { should have_title('Sign Up') }
   end
+
+  describe "profile page" do
+    before { visit user_path(user) }
+
+    it { should have_content(user.name) }
+    it { should have_title(user.name) }
+  end
 end
