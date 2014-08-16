bee4351a5c3d318832221619258a3ed97541764f
Justin Veirs <justinveirs@gmail.com>
2014-08-16 18:48:11 -0400

    adding factorygirl

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -12,6 +12,7 @@ describe "UserPages" do
   end
 
   describe "profile page" do
+    let(:user) { FactoryGirl.create(:user) }
     before { visit user_path(user) }
 
     it { should have_content(user.name) }
