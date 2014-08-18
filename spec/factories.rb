e1c70208a7e1329514d9f2dfb96b151e18c64e0c
Justin Veirs <justinveirs@gmail.com>
2014-08-18 14:25:47 -0400

    found the error after sitting on it for a day

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,7 +1,7 @@
 FactoryGirl.define do
   factory :user do
-    name "justin veirs"
-    email "justin@example.com"
+    name "Example user"
+    email "user@example.com"
     password "foobar"
     password_confirmation "foobar"
   end
