57f0c48a1935a9999d26cda547f9be2945f0b5ef
Justin Veirs <justinveirs@gmail.com>
2014-08-25 13:17:23 -0400

    shit is broke

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -7,7 +7,15 @@ module SessionsHelper
     self.current_user = user
   end
 
+  def signed_in?
+    !current_user.nil?
+  end
   def current_user=(user)
     @current_user = user
+  end
+
+  def current_user
+    rememeber_token = User.digest(cookies[:remember_token])
+    @current_user ||= User.find_by(remember_token: remember_token)
   end
 end
