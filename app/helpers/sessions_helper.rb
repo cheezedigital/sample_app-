a67f62d886aeb3e7a265ce70b54a97782c98b903
Justin Veirs <justinveirs@gmail.com>
2014-08-25 12:33:20 -0400

    working it

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,2 +1,8 @@
 module SessionsHelper
+
+  def sign_in(user)
+    remember_token = User.new_remember_token
+    cookies.permanent[:remember_token] = rememeber_token
+    user.update_attribute[:remember_token]
+  end
 end
