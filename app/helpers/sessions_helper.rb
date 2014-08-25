31ba8a0b0b2df00d08695347e409fc37c588544b
Justin Veirs <justinveirs@gmail.com>
2014-08-25 15:10:28 -0400

    shit is failing all over

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -2,7 +2,7 @@ module SessionsHelper
 
   def sign_in(user)
     remember_token = User.new_remember_token
-    cookies.permanent[:remember_token] = rememeber_token
+    cookies.permanent[:remember_token] = remember_token
     user.update_attribute(:remember_token, User.digest(remember_token))
     self.current_user = user
   end
@@ -19,7 +19,9 @@ module SessionsHelper
     @current_user ||= User.find_by(remember_token: remember_token)
   end
 
-  def signed_out?
-    
+  def signed_out
+    current user update attribute(:remember_token, User.digest(User.new_remember_token))
+    cookies.delete(:remember_token)
+    self.current_user = nil
   end
 end
