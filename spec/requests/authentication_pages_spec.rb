4c67d574c4143790c3c7ad9a02f3aa94454776b5
Justin Veirs <justinveirs@gmail.com>
2014-08-26 11:35:38 -0400

    specs broke

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -27,10 +27,10 @@ describe "Authentication" do
     end
 
 
-    describe "with valid informaiton" do
+    describe "with valid information" do
       let(:user) { FactoryGirl.create(:user) }
       before do
-        fill_in "Email", with: user.email.upcase
+        fill_in "Email",    with: user.email.upcase
         fill_in "Password", with: user.password
         click_button "Sign In"
       end
