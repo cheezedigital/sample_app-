778244b02d7657b4e2fe24c50f734095543ec1f0
Justin Veirs <justinveirs@gmail.com>
2014-09-10 18:33:35 -0400

    settings for specs

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -36,6 +36,7 @@ describe "Authentication" do
 
       it { should have_title(user.name) }
       it { should have_link('Profile',     href: user_path(user)) }
+      it { should have_link('Settings',    href: edit_user_path(user)) }
       it { should have_link('Sign Out',    href: signout_path) }
       it { should_not have_link('Sign In', href: signin_path) }
 
